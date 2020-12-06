15c7932a5593a61b9922d9fe64198a0034678346
Mohit Jangra <mohitjangra6017@gmail.com>
2020-12-06 23:07:24 +0530

    update 1st

--- Assignment 1st Portfolio WebPage/main.py
+++ Assignment 1st Portfolio WebPage/main.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for,request,template
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello Mohit"
-
-@app.route('/login')
-def login():
-    return "Welcome to login Page"
+@app.route('/home')
+def home():
+    return template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
